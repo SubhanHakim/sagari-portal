92df34e0524e754464434c375c64c17704938369
Subhan Hakim <subhanhakim07@gmail.com>
2025-08-22 13:04:37 +0700

    Dashboard sementara

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,31 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-3 gap-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900 flex flex-col justify-center items-center gap-4">
                     <x-application-logo class="w-20 h-20" />
+                    <div class="flex flex-col items-center">
+                        <h2>Nama Aplikasi</h2>
+                        <h2 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, fuga maiores voluptatibus ullam, optio animi in cupiditate saepe omnis, nisi amet neque officiis deserunt incidunt a? Iste blanditiis reiciendis quis!</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex flex-col justify-center items-center gap-4">
+                    <x-application-logo class="w-20 h-20" />
+                    <div class="flex flex-col items-center">
+                        <h2>Nama Aplikasi</h2>
+                        <h2 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, fuga maiores voluptatibus ullam, optio animi in cupiditate saepe omnis, nisi amet neque officiis deserunt incidunt a? Iste blanditiis reiciendis quis!</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex flex-col justify-center items-center gap-4">
+                    <x-application-logo class="w-20 h-20" />
+                    <div class="flex flex-col items-center">
+                        <h2>Nama Aplikasi</h2>
+                        <h2 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, fuga maiores voluptatibus ullam, optio animi in cupiditate saepe omnis, nisi amet neque officiis deserunt incidunt a? Iste blanditiis reiciendis quis!</h2>
+                    </div>
                 </div>
             </div>
         </div>
